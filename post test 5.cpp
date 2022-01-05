a63c0cf85987a07059a3efc52c1c4bb74558a4aa
anindarevioktaviani <92665686+anindarevioktaviani@users.noreply.github.com>
2022-01-05 15:32:42 +0700

    Add files via upload

---
+++ post test 5.cpp
@@ -0,0 +1,18 @@
+#include <ncurses/ncurses.h>
+using namespace std;
+
+int main(){
+	initscr();
+	
+	printw("aninda revi oktaviani\n");
+	
+	printw("2117051099\n");
+	
+	refresh();
+	
+	getch();
+	
+	
+	
+	
+}
